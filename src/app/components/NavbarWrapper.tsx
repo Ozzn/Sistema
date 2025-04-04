7567b5c78516bf403b363cf026fa51000d4f3311
javier01042024 <franciscosira2021@gmail.com>
2025-04-03 21:43:32 -0400

    v1

--- src/app/components/NavbarWrapper.tsx
+++ src/app/components/NavbarWrapper.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import Navbar from "./Navbar";
 
 export default function NavbarWrapper() {
   const pathname = usePathname();
   const hideNavbar = ["/", "/login", "/register"].includes(pathname);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   if (hideNavbar) return null;
-  
-  return <Navbar />;
+
+  return <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />;
 }
