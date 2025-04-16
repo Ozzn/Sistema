f46b20902ed56138e85816f0c736338de2ece952
javier01042024 <franciscosira2021@gmail.com>
2025-04-15 20:56:02 -0400

    Update route.ts

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -1,7 +1,7 @@
 // /app/api/mantenimientos/mis-trabajos/route.ts
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth"; // Asegúrate de tenerlo correctamente configurado
+import { authOptions } from "@/lib/authOptions"; // Asegúrate de tenerlo correctamente configurado
 import prisma from "@/lib/prisma"; // Tu cliente Prisma
 
 export async function GET() {
