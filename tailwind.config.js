05e04ab414534397aaf7bf2bf400c13f661f4f1c
Ozzn <102200861+Ozzn@users.noreply.github.com>
2025-04-15 23:20:51 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // Otros directorios o archivos si es necesario
   ],
   theme: {
     extend: {},
